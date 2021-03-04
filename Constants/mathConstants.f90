b55e785cc17da108701767ff018d45f6c0c7a079
paul-lane <64542047+paul-lane@users.noreply.github.com>
2021-03-04 11:04:15 +0000

    Add files via upload

---
+++ Constants/mathConstants.f90
@@ -0,0 +1,11 @@
+module mathConstants
+
+    ! General use mathematical constants for use in all subroutines
+
+    double precision, parameter :: pi = 3.141592653589793D0
+    double precision, parameter :: boltzmannConstant = 1.38064852D-23
+    double precision, parameter :: avagadro = 6.0221409D23
+
+    public :: pi, boltzmannConstant
+
+end module mathConstants
