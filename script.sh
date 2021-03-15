99e5409b0ccd0ebc83a445c06c72afb8d55cfa96
paul-lane <64542047+paul-lane@users.noreply.github.com>
2021-03-15 10:05:32 +0000

    Add files via upload

--- script.sh
+++ script.sh
@@ -7,6 +7,8 @@ cp Batch001/Inputs/mirrorSpots.inp Inputs/
 cp Batch001/Inputs/overwrite.inp Inputs/
 cp Batch001/Inputs/timingInputs.inp Inputs/
 mkdir Batch001/Outputs
+mkdir Batch001/Outputs/Processing1
+mkdir Batch001/Outputs/Speed
 ./MCScattering_FM.out
 ./TimeSliceBinning.out
 ./StepSizeBinning.out
@@ -20,16 +22,22 @@ mv Outputs/ScatteredCellProfile.txt Batch001/Outputs
 mv Outputs/ScatteredProbed.txt Batch001/Outputs
 mv Outputs/Statistics.txt Batch001/Outputs
 mv Outputs/WheelProfile.txt Batch001/Outputs
-mv Outputs/Abs_001.txt Batch001/Outputs
-mv Outputs/Abs_002.txt Batch001/Outputs
-mv Outputs/Abs_003.txt Batch001/Outputs
-mv Outputs/Abs_004.txt Batch001/Outputs
-mv Outputs/FMabs_001.txt Batch001/Outputs
-mv Outputs/FMabs_002.txt Batch001/Outputs
-mv Outputs/FMabs_003.txt Batch001/Outputs
-mv Outputs/FMabs_004.txt Batch001/Outputs
-mv Outputs/SpeedDistr_001.txt Batch001/Outputs
-mv Outputs/SpeedDistr_002.txt Batch001/Outputs
-mv Outputs/SpeedDistr_003.txt Batch001/Outputs
-mv Outputs/SpeedDistr_004.txt Batch001/Outputs
+mv Outputs/Abs_001.txt Batch001/Outputs/
+mv Outputs/Abs_002.txt Batch001/Outputs/
+mv Outputs/Abs_003.txt Batch001/Outputs/
+mv Outputs/Abs_004.txt Batch001/Outputs/
+mv Outputs/FMabs_001.txt Batch001/Outputs/
+mv Outputs/FMabs_002.txt Batch001/Outputs/
+mv Outputs/FMabs_003.txt Batch001/Outputs/
+mv Outputs/FMabs_004.txt Batch001/Outputs/
+mv Outputs/SpeedDistr_001.txt Batch001/Outputs/
+mv Outputs/SpeedDistr_002.txt Batch001/Outputs/
+mv Outputs/SpeedDistr_003.txt Batch001/Outputs/
+mv Outputs/SpeedDistr_004.txt Batch001/Outputs/
+mv Outputs/Processing1/Output_001.txt Batch001/Outputs/Processing1/
+mv Outputs/Processing1/Output_002.txt Batch001/Outputs/Processing1/
+mv Outputs/Processing1/Output_003.txt Batch001/Outputs/Processing1/
+mv Outputs/Processing1/Output_004.txt Batch001/Outputs/Processing1/
+mv Outputs/Speed/*.* Batch001/Outputs/Speed/
+
 
